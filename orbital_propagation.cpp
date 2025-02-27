f9aac5b440396f4e5354cc731f01420084377694
Eirik Mulder <ecm0115@auburn.edu>
2025-02-27 14:37:12 -0600

    Add initial velocity and position calculations

--- orbital_propagation.cpp
+++ orbital_propagation.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <array>
+#include <memory>
+
+const double R_E = 6378;
+const double MU = 398600;
+
 
 int main() {
   std::cout << "Hello World!" << std::endl;
+  double altitude = 400;
+  double radius = R_E + altitude;
+  double vel = sqrt(MU / radius);
+  std::array<double, 6> y0 = {radius, 0, 0, 0, vel, 0};
+  std::cout << "Initial Radius: " << radius << " [km], Initial Velocity: "
+            << vel << " [km/s]" << std::endl;
 }
